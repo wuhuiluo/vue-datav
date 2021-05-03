89172241cdd374bbd959b52689f16bee54b5fb53
wuhuiluo <510426343@qq.com>
2021-05-03 10:23:03 +0800

    latest commit

--- src/components/TotalUsers/index.vue
+++ src/components/TotalUsers/index.vue
@@ -62,6 +62,49 @@ export default {
             color: "#eee",
           },
         },
+        {
+          type: "custom",
+          stack: "总量",
+          data: [200],
+          renderItem: (params, api) => {
+            const value = api.value(0);
+            const endPoint = api.coord([value, 0]);
+            return {
+              type: "group",
+              position: endPoint,
+              children: [
+                {
+                  type: "path",
+                  shape: {
+                    d: "M1024 255.996 511.971 767.909 0 255.996 1024 255.996z",
+                    x: -5,
+                    y: -20,
+                    width: 10,
+                    height: 10,
+                    layout: "cover",
+                  },
+                  style: {
+                    fill: "#34c946",
+                  },
+                },
+                {
+                  type: "path",
+                  shape: {
+                    d: "M0 767.909l512.029-511.913L1024 767.909 0 767.909z",
+                    x: -5,
+                    y: 10,
+                    width: 10,
+                    height: 10,
+                    layout: "cover",
+                  },
+                  style: {
+                    fill: "#34c946",
+                  },
+                },
+              ],
+            };
+          },
+        },
       ],
     });
   },
