eae3d3f26831c9067edeaac1cacdbaba28f5c1b6
wuhuiluo <510426343@qq.com>
2021-05-04 11:33:21 +0800

    latest commit

--- src/components/SalesView/index.vue
+++ src/components/SalesView/index.vue
@@ -7,6 +7,7 @@
             :default-active="activeIndex"
             @select="onMenuSelect"
             mode="horizontal"
+            class="sales-view-menu"
           >
             <el-menu-item index="1">销售额</el-menu-item>
             <el-menu-item index="2">访问量</el-menu-item>
@@ -19,6 +20,7 @@
               <el-radio-button label="今年" />
             </el-radio-group>
             <el-date-picker
+              size="small"
               range-separator="至"
               end-placeholder="结束日期"
               start-placeholder="开始日期"
@@ -26,7 +28,22 @@
               v-model="date"
               unlink-panels
               :picker-options="pickerOptions"
+              class="sales-view-date-picker"
             />
+          </div>
+        </div>
+      </template>
+      <template>
+        <div class="sales-view-chart-wrapper">
+          <v-chart :options="chartOption" />
+          <div class="sales-view list">
+            <div v-for="item in rankData" :key="item.no" class="list-item">
+              <div :class="['list-item-no', +item.no <= 3 ? 'top-no' : '']">
+                {{ item.no }}
+              </div>
+              <div class="list-item-name">{{ item.naem }}</div>
+              <div class="list-item-money">{{ item.money }}</div>
+            </div>
           </div>
         </div>
       </template>
@@ -73,6 +90,45 @@ export default {
           },
         ],
       },
+      chartOption: {},
+      rankData: [
+        {
+          no: 1,
+          name: "麦当劳",
+          money: "323,234",
+        },
+        {
+          no: 2,
+          name: "麦当劳",
+          money: "323,234",
+        },
+        {
+          no: 3,
+          name: "麦当劳",
+          money: "323,234",
+        },
+        {
+          no: 4,
+          name: "麦当劳",
+          money: "323,234",
+        },
+        {
+          no: 5,
+          name: "麦当劳",
+          money: "323,234",
+        },
+
+        {
+          no: 6,
+          name: "麦当劳",
+          money: "323,234",
+        },
+        {
+          no: 7,
+          name: "麦当劳",
+          money: "323,234",
+        },
+      ],
     };
   },
   methods: {
@@ -88,7 +144,29 @@ export default {
 .sales-view {
   margin-top: 20px;
   .menu-wrapper {
+    position: relative;
     display: flex;
+    .sales-view-menu {
+      width: 100%;
+      padding-left: 20px;
+      .el-menu-item {
+        height: 50px;
+        line-height: 50px;
+        margin: 0 20px;
+      }
+    }
+    .menu-right {
+      position: absolute;
+      right: 20px;
+      top: 0;
+      height: 50px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      .sales-view-date-picker {
+        margin-left: 20px;
+      }
+    }
   }
 }
 </style>
